acb250cd9fd75d0d1d61d8fc4a4558bf2662b6b0
Alisson Daza <103325400+dino-ali@users.noreply.github.com>
2023-04-17 10:34:12 -0500

    tarea laberinto

---
+++ laberinto.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Puntito{
+private:
+    int x;
+    int y;
+    int dir;
+public:
+    Puntito(){
+        x = 0;
+        y = 0;
+        dir = 0;
+    }
+    
+    Puntito(int a, int b){
+        x = a;
+        y = b;
+        
+    }
+    
+    int get_x(){
+        return x;
+    }
+    
+    int get_y(){
+        return y;
+    }
+    
+    void turn_right
+};
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+
+    return 0;
+}
