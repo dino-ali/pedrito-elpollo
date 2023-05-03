a40add59f69370f336757873f617d070f54960a7
Alisson Daza <103325400+dino-ali@users.noreply.github.com>
2023-05-03 10:27:45 -0500

    Create punto6.cpp

---
+++ taller 3/punto6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void permutacion(string sapa, int* indice, int cuenta_indices){
+    if (cuenta_indices == s.length()){
+        
+    }else{
+        bool found = false;
+        int j = 0;
+        for (int i = 0; i < s.length(); i++){
+            found = false;
+            j = 0;
+            while (j < cuenta_indices && !found){
+                if (indices[j] == i){
+                    found = true;
+                }else{
+                    j++;
+                }
+            }
+            
+        }
+    }
+}
+
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!";
+
+    return 0;
+}
