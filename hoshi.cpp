088f0f951e86e4e359039611da72b7fb4d87ae0a
Alisson Daza <103325400+dino-ali@users.noreply.github.com>
2023-05-11 23:13:07 -0500

    hoshi

---
+++ hoshi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void hoshi(){
+    string s; 
+    cout<<"Escriba algo: ";
+    cin >> s;
+    cout<< endl;
+    int c = 1 ; 
+    for( int i = 0; i < s.length(); i++){
+         c = (c*s[i])%1000000;
+         cout<<c<<endl;
+    }
+    cout << "El valor es: " << c << endl;
+}
+
+int main(){
+    hoshi();
+
+    return 0;
+}
